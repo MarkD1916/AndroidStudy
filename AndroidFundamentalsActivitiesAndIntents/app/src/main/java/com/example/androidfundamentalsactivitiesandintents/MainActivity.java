006a44bc16d48750ec3b30cac7dd34df05598d00
mark <vmakd1916@gmail.com>
2020-07-19 17:08:51 +0300

    add some "communication" with two activities, but I don t like code

--- AndroidFundamentalsActivitiesAndIntents/app/src/main/java/com/example/androidfundamentalsactivitiesandintents/MainActivity.java
+++ AndroidFundamentalsActivitiesAndIntents/app/src/main/java/com/example/androidfundamentalsactivitiesandintents/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidfundamentalsactivitiesandintents;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +17,17 @@ public class MainActivity extends AppCompatActivity {
     private Intent goToSecAct;
 
     private EditText mMessageEditText;
+    private TextView mReplyText;
     public static final String EXTRA_MASSAGE = "Send";
+
+    public static final int TEXT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMessageEditText = findViewById(R.id.editText_main);
+        mReplyText = findViewById(R.id.text_reply);
     }
 
     public void launchSecondActivity (View view){
@@ -29,7 +35,20 @@ public class MainActivity extends AppCompatActivity {
         String message = mMessageEditText.getText().toString();
         goToSecAct = new Intent(this,SecondActivity.class);
         goToSecAct.putExtra(EXTRA_MASSAGE,message);
-        startActivity(goToSecAct);
+        //startActivity(goToSecAct);
+        startActivityForResult(goToSecAct,TEXT_REQUEST);
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==TEXT_REQUEST){
+            if (resultCode==RESULT_OK){
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyText.setVisibility(View.VISIBLE);
+                mReplyText.setText(reply);
+            }
+        }
+    }
 }
